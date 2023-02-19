8aa783c91fa795fd5f53f6975b508454516daaae
Anirudh V Gubbi <anirudhvgubbi@gmail.com>
2023-02-19 18:43:48 +0530

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Anirudh V Gubbi on 19/02/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
